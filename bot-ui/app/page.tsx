d6f5f155b8bb4bec8a90c142cce04a9957aa3a60
Dan Dumitru <dandumitru27@gmail.com>
2025-10-14 17:42:11 +0300

    Added react-chatbotify; basic ChatBot, cleaned up

--- bot-ui/app/page.tsx
+++ bot-ui/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import ChatBot from "react-chatbotify";
+
 export default function Home() {
-  return <div>Hello!</div>;
+  const flow = {
+    start: {
+      message: "Salut! Cum te cheama?",
+      path: "end",
+    },
+    end: {
+      message: (params: any) => `Ma bucur de cunostinta, ${params.userInput}!`,
+      chatDisabled: true,
+    },
+  };
+
+  return (
+    <ChatBot
+      settings={{
+        general: { embedded: true },
+        footer: { text: "" },
+        header: { title: "Claudiu", showAvatar: false },
+      }}
+      flow={flow}
+    />
+  );
 }
