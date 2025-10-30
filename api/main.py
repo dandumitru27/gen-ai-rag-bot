1c8ea5ea1a67f4e40877a16a723ea43ee8daf6e3
Dan Dumitru <dandumitru27@gmail.com>
2025-10-30 16:30:01 +0200

    bot-ui calling api; CORS config

--- api/main.py
+++ api/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from init_llm import init_llm
 from models import PostRequest, PostResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
--- bot-ui/app/page.tsx
+++ bot-ui/app/page.tsx
@@ -3,14 +3,37 @@
 import ChatBot from "react-chatbotify";
 
 export default function Home() {
+  async function fetchData(human_message: string) {
+    try {
+      const body = { human_message: human_message };
+
+      const response = await fetch("http://127.0.0.1:8000/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+
+      return data.ai_message;
+    } catch (error) {
+      return (
+        "Eroare de server. A fost inregistrata si va fi remediata in curand. " +
+        "Va rugam sa incercati din nou mai tarziu."
+      );
+    }
+  }
+
   const flow = {
     start: {
-      message: "Salut! Cum te cheama?",
-      path: "end",
+      message: "Buna ziua! Cu ce va pot ajuta?",
+      path: "loop",
     },
-    end: {
-      message: (params: any) => `Ma bucur de cunostinta, ${params.userInput}!`,
-      chatDisabled: true,
+    loop: {
+      message: async (params: any) => await fetchData(params.userInput),
+      path: "loop",
     },
   };
 
